aab9b2701ab0918c2bdac8dad3316a0492a05dfd
uniquexcel <uniquexcelf@gmail.com>
2022-08-08 13:13:10 -0700

    This repository contains some codes i wrote for personal development and learning at my pace.

---
+++ Getting_weather_information.py
@@ -0,0 +1,13 @@
+# This program helps users get weather information from their specified location.
+import requests
+from pprint import pprint
+
+API_Key = 'db53c569d4657d69db0a8d95d8027d12'
+
+city = input("Enter a City: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
